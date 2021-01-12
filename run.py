978cf3054233567277e2a5a885bf262f1d0a0cb3
miahcaine <miahc@Miahs-MacBook-Pro.local>
2021-01-12 14:49:17 -0500

    create database on run

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-from prod_break import app
+import os
+from prod_break import app, db
 
 if __name__ == "__main__":
+    if "site.db" not in os.listdir("prod_break"):
+        db.create_all()
     app.run(debug=True)
